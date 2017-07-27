ba1cabb3c1f9923c1347fb8b84170057c23f475f
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-07-27 16:40:48 -0400

    implemented button action

--- ObjectiveCPractice/ViewController.m
+++ ObjectiveCPractice/ViewController.m
@@ -20,18 +20,28 @@
     // Create the label and set its frame
     UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(50, 50, 200, 50) ];
     
-    // Set the text on the label
+    // Set the text on the labels
     label.text = @"Hello world! * 2";
+    self.helloLabel.text = @"default text"; // iboutlet
+    
+    // Set button label
+    [self.button setTitle:@"Click Me" forState:UIControlStateNormal];
     
     // Add the label to the view
     [self.view addSubview:label];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)handleClick:(id)sender {
+    self.helloLabel.text = self.textfield.text;
+}
+
+- (void)touchesBegan: (NSSet *)touches withEvent:(UIEvent *)event {
+    [self.textfield resignFirstResponder];
+}
 
 @end
